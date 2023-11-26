509bb5fa2fae881e004f2f27c4d34dd55add46b4
vinuabrahams <vinuabra@cgd.ws>
2023-11-26 19:06:56 +0530

    update: usage.py function

--- usage.py
+++ usage.py
@@ -1,166 +1,148 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-os.environ['XLA_FLAGS']='--xla_gpu_cuda_data_dir=/home/kailash/anaconda3/envs/Classifier_env/lib'
-
-import boto3
-
-my_session = boto3.session.Session()
-my_region = my_session.region_name
-print(my_region)
-import numpy as np
-import pandas as pd
+import glob
 import argparse
+import mlflow
 from classifier.loader import LoadDataset
 from classifier.augopt import Mapping
 from classifier.arch import Model
-from classifier.evaluate import Evaluation
 from classifier.fitcompile import Training
-import matplotlib.pyplot as plt
-import subprocess
-#export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
-#export AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
-#check whether GPU is being used
 import tensorflow as tf
+from classifier.utils import plot_loss, augmented_images, cm_binary
+from sklearn.metrics import (confusion_matrix, ConfusionMatrixDisplay,
+                             classification_report)
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 gpus = tf.config.list_physical_devices('GPU')
 print(gpus)
 
-import mlflow
-from mlflow.models.signature import infer_signature
-
-gpus = tf.config.list_physical_devices('GPU')
-print(gpus)
-#check whether GPU is being used
+# check whether GPU is being used
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 
-parser =  argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
 
-parser.add_argument("-pm","--pretrained_model",
+parser.add_argument("-pm", "--pretrained_model",
                     help="one of the pretrained models from keras.applications",
                     default='MobileNetV3Large')
 
+parser.add_argument("-rn", "--run_name",
+                    help="name to be assigned to mlflow run",
+                    required=True)
+
+parser.add_argument("-v", "--version",
+                    help="assign the version number",
+                    required=True)
+
 args = vars(parser.parse_args())
 trained_model = args["pretrained_model"]
+run_name = args["run_name"]
+version = args["version"]
+batch_size = 2
+image_height, image_width = (1024, 1024)
+channels = 3
 
+# keep the training images in Dataset folder
+load_data = LoadDataset(fpath='Dataset/dataset',
+                        batch_size=2,
+                        split=0.2,
+                        image_size=[image_height, image_width])
 
-#keep the training images in Dataset folder
-l = LoadDataset(fpath='Datasets/Kitchenwares',
-                batch_size = 32,
-                split=0.2,
-                image_size=[400,400])
+train_ds = load_data.load_train()
 
-train_ds = l.load_train()
-
-valid_ds = l.load_valid()
+valid_ds = load_data.load_valid()
 
 print(train_ds.class_names)
 
+augmented_images(train_ds, batch_size=batch_size)
+
 # test_ds = l.load_test('augmented_images')
 
-m = Mapping(image_size=[224,224],
-            prefetch=False,model_name = trained_model)
+m = Mapping(image_size=[image_height, image_width],
+            prefetch=False, model_name=trained_model)
 
-train_ds = m.mapping(ds =train_ds,
-                     augment=True,
-                    shuffle=False)
+train_ds = m.mapping(ds=train_ds,
+                     augment=False,
+                     shuffle=False)
 
-valid_ds = m.mapping(ds = valid_ds, 
-                    augment=False,
-                    shuffle=False)
+valid_ds = m.mapping(ds=valid_ds, 
+                     augment=False,
+                     shuffle=False)
 
-# check the augmented images
-# plt.figure(figsize=(10, 10))
-# for images, labels in train_ds.take(1):
-#   for i in range(4):
-#     if not os.path.exists("augmented_images"):
-#       os.mkdir("augmented_images")
-
-#     tf.keras.utils.save_img(f"augmented_images/image{i}.png", images[i])
-
-
-#Defining model architecture
-#for a custom architecture load the model and from the python file and continue
-
-m = Model(input_shape=(224,224,3),
-            ndenselayers=[64,32,16],
-            activation = 'relu', 
-            dropout=0.2,
-            prediction_classes =6,
-            pretrained_model=trained_model)
+ndenselayers = [16]
+m = Model(input_shape=(image_height, image_width, channels),
+          ndenselayers=ndenselayers,
+          activation='relu',
+          dropout=0.2,
+          prediction_classes=1,
+          pretrained_model=trained_model)
 
 model = m.arch()
 
+mod = Training(model=model)
 
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
-# model.summary()
-
-
-md = Training(model=model)
-
-
-track ="ec2-13-232-120-157.ap-south-1.compute.amazonaws.com"
-# mlflow.set_tracking_uri("sqlite:///mlflow.db")
-mlflow.set_tracking_uri(f"http://{track}:5000")
 print(f"track uri:{mlflow.get_tracking_uri()}")
 
 
-mlflow.set_experiment("Kitch_classification")
+mlflow.set_experiment("First_page_classification")
 
-with mlflow.start_run():
-   mlflow.set_tag("ml_engineer","kailash")
+with mlflow.start_run(run_name=run_name):
+    mlflow.set_tag("ml_engineer", "kailash")
+    opt = 'Adam'
+    lr = 0.00001
+    epoch = 50
+    mlflow.log_params({"image_size": (image_height, image_width, channels),
+                       "batch_size": batch_size,
+                       "cnn_backbone": trained_model,
+                       "dense_layers": ndenselayers
+                       })
 
-   opt = 'Adam'
-   lr= 0.001
-   epoch =25
-   mlflow.log_params({"learning_rate":lr,
-                     "epoch":epoch,
-                     "optimiser":opt}
-   )
+    mlflow.log_params({"learning_rate": lr,
+                       "epoch": epoch,
+                       "optimiser": opt})
 
+    mod.compile(loss=tf.keras.losses.BinaryCrossentropy(), optimizer=opt, 
+                lrate=lr, metrics='accuracy')
+  
+    his, model = mod.fit(epoch, train_ds=train_ds,
+                         valid_ds=valid_ds,
+                         model_name=trained_model+"_V"+version)
+    # mlflow.tensorflow.log_model(model,
+    #                             artifact_path="model")
+    tf.keras.utils.plot_model(model, show_shapes=True,
+                              to_file="model.png")
+    mlflow.log_artifact("model.png")
+    mlflow.log_metrics({"val_loss": min(his.history['val_loss']),
+                        "val_accuracy": min(his.history['val_accuracy']),
+                        "train_loss": min(his.history['loss']),
+                        "train_accuracy": min(his.history['accuracy'])
+                        })
 
-   md.compile(loss='sparse_categorical_crossentropy',optimizer=opt,lrate = lr, metrics='accuracy')
-   his,model = md.fit(epoch,train_ds=train_ds,valid_ds=valid_ds,model_name=trained_model)
+    plot_loss(his)
+    for file in glob.glob("augmented_images"):
+        mlflow.log_artifact(file)
+    mlflow.log_artifact("accuracy_curve.png")
+    mlflow.log_artifact("loss_curve.png")
 
+    val_labels, pred = cm_binary(valid_ds, model)
+    cm = confusion_matrix(val_labels, pred)
+    result = cm.ravel()
+    print(result)
+    if len(result) == 4:
+        t_n, f_p, f_n, t_p = result
 
-   # mlflow.log_params(model.history
-   # )
+        mlflow.log_metric("tn", t_n)
+        mlflow.log_metric("fp", f_p)
+        mlflow.log_metric("fn", f_n)
+        mlflow.log_metric("tp", t_p)
+    else:
+        mlflow.log_metric("cm", result)
 
-   #mlflow.log_artifact(f"models/{trained_model}.h5",artifact_path="models_tensorflow")
-   # model_signature = infer_signature(train_ds,model.predict(train_ds))
-   mlflow.tensorflow.log_model(model,artifact_path="models")
-
-   mlflow.log_metrics({
-                        "loss":min(his.history['val_loss']) }           
-                           )
-
-
-# opt = 'Adam'
-# lr= 0.0001
-# epoch =5
-# mlflow.tensorflow.autolog()
-# mlflow.ten
-
-# md.compile(loss='sparse_categorical_crossentropy',optimizer=opt,lrate = lr, metrics='accuracy')
-# metrics = md.fit(epoch,train_ds=train_ds,valid_ds=valid_ds)
-
-
-
-
-
-
-
-
-
-
-
-
-# print('The accuracy of model  :',metrics.accuracy)
-# print('The precision of model :',metrics.precision)
-# print('The recall of model    :',metrics.recall)
-
-
-# e = Evaluation(model,valid_ds)
-
-# e.confusion_matrix()
-
-# e.misclassified()
+    disp = ConfusionMatrixDisplay(confusion_matrix(val_labels, pred))
+    disp.plot().figure_.savefig('confusion_matrix.png')
+    mlflow.log_artifact('confusion_matrix.png')
+    mlflow.log_dict(classification_report(val_labels,
+                                          pred, output_dict=True),
+                                          "classification_report.json")
